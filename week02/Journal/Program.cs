eda800d5da3ce63401ea9e270e5006da8d3bdd84
CaioSEC <caiosecond13@gmail.com>
2025-03-17 01:32:48 -0300

    journal updated

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.Write("Please select one option.");
+        Console.WriteLine();
+        Console.Write("1. Write ");
+        Console.WriteLine();
+        Console.Write("2. Display ");
+        Console.WriteLine();
+        Console.Write("3. Save ");
+        Console.WriteLine();
+        Console.Write("4. Load ");
+        Console.WriteLine();
+        Console.Write("5. Quit ");
+        Console.WriteLine();
+        Console.Write("What would you like to do? ");
+        string options = Console.ReadLine();
+
+        if (options == "1")
+        {
+            Console.WriteLine("What did i learned today?");
+            string answer = Console.ReadLine();
+        }
     }
 }
+
+
