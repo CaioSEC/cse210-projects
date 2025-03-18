c146c91d204108ed7403ea61734484fc3eb88ceb
CaioSEC <caiosecond13@gmail.com>
2025-03-18 01:11:06 -0300

    journal program updated

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,26 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please select one option.");
-        Console.WriteLine();
-        Console.Write("1. Write ");
-        Console.WriteLine();
-        Console.Write("2. Display ");
-        Console.WriteLine();
-        Console.Write("3. Save ");
-        Console.WriteLine();
-        Console.Write("4. Load ");
-        Console.WriteLine();
-        Console.Write("5. Quit ");
-        Console.WriteLine();
-        Console.Write("What would you like to do? ");
-        string options = Console.ReadLine();
+        string answer = ""; // Store the answer globally
 
-        if (options == "1")
+        while (true)
         {
-            Console.WriteLine("What did i learned today?");
-            string answer = Console.ReadLine();
-        }
+            Console.WriteLine("Please select one option:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? Please insert the number: ");
+            
+            string options = Console.ReadLine();
+
+            if (options == "1")
+            {
+                Console.WriteLine("What did I learn today?");
+                answer = Console.ReadLine();
+            }
+            else if (options == "2")
+            {
+                Console.WriteLine("You wrote: " + (answer == "" ? "Nothing" : answer));
+                Console.WriteLine();
+            }
+            else if (options == "5")
+            {
+                break;
+            }
+        }   
     }
 }
 
